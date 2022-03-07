2d15fdd292bf95c01a0288c73914f72b93c22f80
Kevin Owens III <krowens3rd@gmail.com>
2022-03-06 17:56:15 -0800

    ✅ use new keypair generated for everyone

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import kp from "./keypair.json";
 const { SystemProgram, Keypair } = web3;
 
 // Create a keypair for the account that will hold the GIF data.
-let baseAccount = Keypair.generate();
+const arr = Object.values(kp._keypair.secretKey);
+const secret = new Uint8Array(arr);
+const baseAccount = web3.Keypair.fromSecretKey(secret);
 
 // Get our program's id from the IDL file.
 const programID = new PublicKey(idl.metadata.address);
